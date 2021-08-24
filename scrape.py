fdbdd33c0d7aa4938858aa3a61860e554d320928
BernieTheBear <balmanzar883@gmail.com>
2021-08-24 14:08:49 -0400

    Updated Scrape
    Scrape module can has ebay and CL search functionality
    Scrape module also retrieves data from target elements as list for further processing.
    Currently working on BAT functionality
    
    BAT functionality goal:
    use BAT to populate eras/chasis for given model
    Use populated era to display to user which era to choose for the given model.
    Refine search for that given era by making refined request to BAT
    Target and click 'show more' until element is no longer visible (ElementNotVisible Exception) -> this is to load all previous listings
    Collect year and price from each populated previous listing item (all car listings)

--- scrape.py
+++ scrape.py
@@ -12,22 +12,26 @@ https://www.selenium.dev/documentation/webdriver/web_element/
 
 https://stackoverflow.com/questions/34315533/can-i-find-an-element-using-regex-with-python-and-selenium
 https://stackoverflow.com/questions/12323403/how-do-i-find-an-element-that-contains-specific-text-in-selenium-webdriver-pyth
+
+https://stackoverflow.com/questions/45990851/how-do-i-iterate-through-a-webelements-list-with-python-and-selenium/46001881
 """
 
 
 from threading import Thread
+from typing import ClassVar
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementNotVisibleException
 import time
 
 
 
 
 
-make = "audi"
-chassis ="r8"
-model ="r8"
+make = "Mercedes"
+chassis =""
+model ="e63"
 # 98-03 for m5
 
 
@@ -42,9 +46,8 @@ print(vehicle)
 # Create a new chromedriver
 driver = webdriver.Chrome(executable_path=r'C:\Users\balma\Documents\Programming\chromedriver.exe')
 
-
-
-
+# ===========================================
+# :::::: BEGIN WIKI SECTION 
 # """
 # wikiString = f"https://en.wikipedia.org/wiki/{make}_{model}"
 # print(wikiString)
@@ -53,31 +56,85 @@ driver = webdriver.Chrome(executable_path=r'C:\Users\balma\Documents\Programming
 # specific_model_production = driver.find_elements_by_xpath("//*[contains(text(),'E39 M5 (')]")
 # for i in specific_model_production:
 #     r = i.text
-    
+# :::::: END WIKI SECTION  
+
+# ===========================================
+# :::::: BEGIN EBAY SECTION 
+# driver.get("https://www.ebay.com/b/Cars-Trucks/6001/bn_1865117")
+# driver.implicitly_wait(15)
+
+# #enter model 
+# search_box = driver.find_element_by_css_selector('#gh-ac')
+# # WebDriverWait(driver,10)
+# time.sleep(1)
+# search_box.send_keys(vehicle + Keys.RETURN)
 
 
-# # Go to ebay
-driver.get("https://www.ebay.com/b/Cars-Trucks/6001/bn_1865117")
-driver.implicitly_wait(15)
+# # this gets prices of all cars on page
+# prices = []
+# test = driver.find_elements_by_class_name('s-item__price')
+# for i in test:
+#     price = (i.get_attribute('innerHTML'))
+#     prices.append(price)
+# time.sleep(1)
+# print(prices)
 
-#enter model 
-search_box = driver.find_element_by_css_selector('#gh-ac')
-# WebDriverWait(driver,10)
-time.sleep(1)
-search_box.send_keys(vehicle + Keys.RETURN)
-
-
-# this gets prices of all cars on page
-prices = []
-test = driver.find_elements_by_class_name('s-item__price')
-for i in test:
-    price = (i.get_attribute('innerHTML'))
-    prices.append(price)
-
-time.sleep(1)
-print(prices)
+# :::::: END EBAY SECTION 
+# ===========================================
 
 
+# :::::: BEGIN CRAIGLIST SECTION
+# search route option 1 - longer and more human-like
+# driver.get('https://miami.craigslist.org/mdc/')
+# CL_searchBar = driver.find_element_by_css_selector('#query')
+# time.sleep(2)
+# CL_searchBar.send_keys(vehicle + Keys.RETURN)
+
+# #search route option 2 - more direct
+# driver.get(f'https://miami.craigslist.org/d/cars-trucks/search/mdc/cta?query={make}%20{model}&sort=rel')
+
+# CL_prices = []
+# CL_items = driver.find_elements_by_class_name('result-info')
+# for item in CL_items:
+#     description = item.find_element_by_class_name('result-heading').get_attribute('innerText')
+#     price = item.find_element_by_class_name('result-price').get_attribute('innerText')
+#     print(f" {description}:{price}")
+
+# :::::: END CRAIGLIST SECTION
+# ===========================================
+# :::::: BEGIN BAT SECTION
+driver.get('https://bringatrailer.com/')
+
+#this is to get passed "show notifications prompt"
+# driver.send_keys(Keys.TAB)
+# driver.send_keys(Keys.TAB)
+# driver.send_keys(Keys.RETURN)
+
+driver.find_element_by_class_name('search-open').click()
+time.sleep(2)
+search_bar = driver.find_element_by_class_name('search-terms')
+search_bar.send_keys(vehicle + Keys.RETURN)
+try:
+    show_more = driver.find_element_by_class_name('page-numbers')
+    show_more.click()
+
+except ElementNotVisibleException:
+    pass
+
+
+# 
+model_list = driver.find_elements_by_class_name('previous-listing previous-listing-notext')
+for model in model_list:
+    model_name = driver.find_element_by_className('previous-listing-image-link').get_Attribute('alt')
+    print(model_name)
+
+
+
+"""
+driver.find_by-xpath('/html/body/div[2]/div[2]/div/div/div[1]).get_attributes
+
+"""
+# :::::: END BAT SECTION
 
 # # Get the webelement of the text input box
 # search_box = driver.find_element_by_name("q")
