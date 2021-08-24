3c8e6d86d909fa70fe56e842f5e9f8eaf2a6474b
BernieTheBear <balmanzar883@gmail.com>
2021-08-24 01:17:31 -0400

    Add files via upload
    Modified form in home template to send data
    Modified routing to extract request parameters and pass to corresponding template

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
+
+# https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+# https://stackoverflow.com/questions/52596200/badrequestkeyerror
+
 from flask import Flask,render_template,request
-from scrape import scrape
+# from scrape import scrape
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-   return render_template('base.html')
+   return render_template('home.html')
 
-@app.route('/search')
+@app.route('/search',methods=['GET','POST'])
 def search():
-   return scrape()
-   
+   if request.method == 'POST':
+      t = request.get_data('searchTerm')
+      return render_template('data.html',t=t)
+   else:
+      return render_template('base.html')
+
 @app.route('/about')
 def about():
    return render_template('data.html')
