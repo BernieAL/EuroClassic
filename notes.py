8624f524a7e6cecedaeb1e8327703373c28347ef
BernieAL <balmanzar883@gmail.com>
2021-09-11 11:42:55 -0400

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,17 @@
+"""
+
+
+
+Can we run mulitple instances of webdriver at the same time in order to hit the data sources at the same exact time.
+Ex. Can we hit CL, Ebay, and BAT at the same time
+
+The idea for implementation would be to have 3 different scripts with 3 difference webdriver instances
+Then from the main app.py, import and run the 3 scripts at the same time.
+
+
+
+
+
+
+
+"""
