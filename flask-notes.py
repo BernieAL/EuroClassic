c231528f8f8796143c18de88e31d6acd3e07d5d2
BernieAL <balmanzar883@gmail.com>
2021-09-11 11:42:48 -0400

    Add files via upload

---
+++ flask-notes.py
@@ -0,0 +1,9 @@
+"""
+
+In python, a subdirectory that includes a __init__.py
+file is considred a package and can be imported
+
+When package is imported, the __init__.py executes and defines what symbols
+the package exposes to the outside world
+
+"""
