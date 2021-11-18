21fe7312fea20a1a6add970f5fd4e3a6ae380906
BernieAL <balmanzar883@gmail.com>
2021-11-18 15:05:31 -0500

    Form acces working in search route handler  - And

--- euro-classic-app/__init__.py
+++ euro-classic-app/__init__.py
@@ -1,41 +1,64 @@
 
 # https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
 # https://stackoverflow.com/questions/52596200/badrequestkeyerror
+# https://stackoverflow.com/questions/42499535/passing-a-json-object-from-flask-to-javascript
+# https://dev.to/brunooliveira/flask-series-part-13-moving-our-recipes-data-source-to-the-server-4lm8
+# https://stackabuse.com/integrating-mongodb-with-flask-using-flask-pymongo/
+# https://github.com/akmamun/mvc-flask-pymongo
 
+
+from dns import message
 from flask import Flask,render_template,request
+from flask_pymongo.wrappers import MongoClient
+import pymongo
 # from scrape import test
-from config import Config
+# from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import json
+from flask_pymongo import PyMongo
 # from data_processing_scripts import handle_data
 # from models import Car
 
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-if db:
-   print('connected to db successfully')
-migrate = Migrate(app,db)
+# # app.config.from_object(Config)
+# client = MongoClient(uri="mongodb+srv://admin:kirklandexpo@cluster0.7sxfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# print(client.test_database)
+
+
+
+# t = Car(2003,"BMW","M5","E39","0")
+# print(t)
+
+
 
 # #this calls function from data_processing_scripts which handles all the collecting and cleaning of data
 # handle_data()
+
+
+
+
 
 
 @app.route('/')
 def homepage():
 
    #load full directory of cars and pass to template for autocomplete
-   car_directory = ['bmw e39 m5', '2003 bmw e39 m5', '2001 bmw e39 m5']
+   car_directory = ['2000 BMW E39 m5', '2003 bmw e39 m5', '2001 bmw e39 m5']
    return render_template('home.html',car_directory=json.dumps(car_directory))
 
-@app.route('/search',methods=["GET","POST"])
+@app.route('/search',methods=['GET','POST'])
 def search():
 
+   
+  
    if request.method == 'POST':
-      car = request.form
+      car = request.form['car']
+      
+
+      
 
       # hit backend API to retrieve results for the specific car
       # Return results to here and pass this to template to be rendered
--- euro-classic-app/config.py
+++ euro-classic-app/config.py
@@ -1,8 +1,7 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('mongodb+srv://admin:kirklandexpo@cluster0.7sxfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# class Config(object):
+#     # ...
+#    "mongodb+srv://admin:kirklandexpo@cluster0.7sxfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    
