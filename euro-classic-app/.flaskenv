a6fa8509eee61cb413f16b64263059254fb91ac7
BernieAL <balmanzar883@gmail.com>
2021-12-07 17:05:09 -0500

    mongodb  added- successfully connected to db

---
+++ euro-classic-app/.flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP = euro-classic-app
+FLASK_ENV = development
+
--- euro-classic-app/__init__.py
+++ euro-classic-app/__init__.py
@@ -6,32 +6,44 @@
 # https://stackabuse.com/integrating-mongodb-with-flask-using-flask-pymongo/
 # https://github.com/akmamun/mvc-flask-pymongo
 
+# https://www.mongodb.com/community/forums/t/keep-getting-serverselectiontimeouterror/126190/18
+# https://stackabuse.com/integrating-mongodb-with-flask-using-flask-pymongo/
+# https://www.mongodb.com/compatibility/setting-up-flask-with-mongodb
 
 from dns import message
-from flask import Flask,render_template,request
+from flask import Flask, config,render_template,request
 from flask_pymongo.wrappers import MongoClient
 import pymongo
-# from scrape import test
-# from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import json
 from flask_pymongo import PyMongo
+
+# from scrape import test
+# from config import Config
+
+
 # from data_processing_scripts import handle_data
 # from models import Car
+
+import os
+import configparser
 
 
 
 app = Flask(__name__)
-# # app.config.from_object(Config)
-# client = MongoClient(uri="mongodb+srv://admin:kirklandexpo@cluster0.7sxfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-# print(client.test_database)
+app.config['MONGO_URI'] = "mongodb+srv://admin:s15koukie39@cluster0.7sxfp.mongodb.net/EURO_CLASSIC?retryWrites=true&w=majority"
+#set up mongodb
+mongodb_client = PyMongo(app) 
+db = mongodb_client.db
 
 
-
-# t = Car(2003,"BMW","M5","E39","0")
-# print(t)
-
+# db.cars.insert_many([
+#       { "year": 2001, "make": "BMW", "Model": "M5", "Chassis":"E39"},
+#       { "year": 1984, "make": "BMW", "Model": "M3", "Chassis":"E30"},
+#       { "year": 2007, "make": "BMW", "Model": "M5", "Chassis":"E60"},
+#       { "year": 2003, "make": "BMW", "Model": "M5", "Chassis":"E39"},
+# ])
 
 
 # #this calls function from data_processing_scripts which handles all the collecting and cleaning of data
@@ -46,8 +58,19 @@ app = Flask(__name__)
 def homepage():
 
    #load full directory of cars and pass to template for autocomplete
-   car_directory = ['2000 BMW E39 m5', '2003 bmw e39 m5', '2001 bmw e39 m5']
-   return render_template('home.html',car_directory=json.dumps(car_directory))
+   # car_directory = ['2000 BMW E39 m5', '2003 bmw e39 m5', '2001 bmw e39 m5']
+   
+   # user_collection = db.todos.drop()
+   #hit db from here and load in all car brands and chasis
+   query = [db.cars.aggregate("Make")]
+   car_directory = db.cars.find({make.data})
+   for i in car_directory:
+      print (i)
+   #retrieve car brands and send as object to be iterated through in template
+   #is there a way to load models once a brand has been selected so that they show up when model tab is selected
+
+
+   return render_template('home.html',car_directory=car_directory)
 
 @app.route('/search',methods=['GET','POST'])
 def search():
@@ -56,7 +79,13 @@ def search():
   
    if request.method == 'POST':
       
-      print(request.form)
+      year = request.form['Year']
+      make = request.form['Make']
+      model = request.form['Model']
+
+      print(year)
+      print(make)
+      print(model)
       # hit backend API to retrieve results for the specific car
       # api_response = handle_data(car)
 
@@ -97,7 +126,6 @@ def account():
 
 
 if __name__ == "__main__":
+   
     app.run(debug=True)
-
-
-from . import models
+# from . import models
