c69ff3ff8d39db23dbdd348e50e795abd4396d87
Bernie Almanzar <balmanzar883@gmail.com>
2023-12-18 23:02:36 -0500

    successfully destructured current_listings into seperate array with target values

--- frontend/my-app/src/components/Graphs.js
+++ frontend/my-app/src/components/Graphs.js
@@ -12,8 +12,13 @@ export default function Graphs({recievedData}){
     },[recievedData])
 
     /* 
-        Destructure Rec'd Data obj
-        into indiv variables
+        Destructure Rec'd Data obj into indiv variables
+
+        returned obj looks like this:
+        
+        {'all_sales_records': [('4c9f93d8-2be1-4f60-b80b-f7eeb96b8f6e', 2010, 'AUDI', 'R8', 108846.48, datetime.date(2023, 10, 27))], 'current_records': [('ece9ef7d-b2de-4548-b84b-ddda25bc0bd9', 2010, 'AUDI', 'R8', 85484.0), ('54b95bd7-e161-482b-ad5f-729f33b68761', 2010, 'AUDI', 'R8', 91654.0)], 'sold_stats': [], 'current_stats': []} 
+
+        
     */
    
 
@@ -33,9 +38,15 @@ export default function Graphs({recievedData}){
     })
     console.log(sales_data_for_graph)
 
-    const current_listing_data_for_graph = current_records((record)=>{
-        return {}
+    const current_listing_data_for_graph = current_records.map((record)=>{
+        return {
+            year:record[1],
+            make:record[2],
+            model:record[3],
+            list_price:record[4]
+        }
     })
+    console.log(current_listing_data_for_graph)
 
     // need to get indiv values off each record returned in each array
     // This means, get the 
