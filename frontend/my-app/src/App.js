17ad4088e40b418be5cc9bc7c9b6b273127a3272
Bernie Almanzar <balmanzar883@gmail.com>
2024-03-13 13:51:48 -0400

    constructing listing card component - moved html out of App.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -5,6 +5,7 @@ import SearchForm from "./components/SearchForm"
 import Graphs from "./components/Graphs/Graphs"
 import VehNotFound from "./components/VehNotFound"
 import Navbar from "./components/UI/Navbar/Navbar"
+import ListingCard from "./components/UI/ListingCard/ListingCard"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -54,156 +55,11 @@ export default function App(){
            
             <div className="jumbotron">test</div>
 
-
+        
             <div 
             className="search_form_wrapper"><SearchForm onDataSubmit={handleDataFromSearchForm}/></div>
             <div className="card_wrapper">
-            
-                <div className="listing_card">
-                    <div className="listing_card_content">
-                        <div className="listing_card_img_container">
-                            <img className="listing_card_img" src={mclarenp1} alt="" />
-                        </div>
-                        <div className="listing_card_icon_stats">
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            
-                            
-                           
-                            {/* <p className="sold_change_30_days">change is sold transactions over last 30 days compared to last 30 days</p>
-                            <p className="price_avg_30_days">numerical dollar value of change is avg listing price compared to last 30 days</p> */}
-                        </div>
-                        <button className="view_result">View Result Set</button>
-                    </div>
-                </div>
-                <div className="listing_card">
-                    <div className="listing_card_content">
-                        <div className="listing_card_img_container">
-                            <img className="listing_card_img" src={mclarenp1} alt="" />
-                        </div>
-                        <div className="listing_card_icon_stats">
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            
-                            
-                            
-                           
-                            {/* <p className="sold_change_30_days">change is sold transactions over last 30 days compared to last 30 days</p>
-                            <p className="price_avg_30_days">numerical dollar value of change is avg listing price compared to last 30 days</p> */}
-                        </div>
-                        <button className="view_result">View Result Set</button>
-                    </div>
-                </div>               
-                <div className="listing_card">
-                    <div className="listing_card_content">
-                        <div className="listing_card_img_container">
-                            <img className="listing_card_img" src={mclarenp1} alt="" />
-                        </div>
-                        <div className="listing_card_icon_stats">
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            
-                            
-                           
-                            {/* <p className="sold_change_30_days">change is sold transactions over last 30 days compared to last 30 days</p>
-                            <p className="price_avg_30_days">numerical dollar value of change is avg listing price compared to last 30 days</p> */}
-                        </div>
-                        <button className="view_result">View Result Set</button>
-                    </div>
-                </div>
-                <div className="listing_card">
-                    <div className="listing_card_content">
-                        <div className="listing_card_img_container">
-                            <img className="listing_card_img" src={mclarenp1} alt="" />
-                        </div>
-                        <div className="listing_card_icon_stats">
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            <div className="listings_change_indicator" 
-                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
-                                <div className="listing_bottom_icons"> 
-                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
-                                    <h2 className="literal_value_change">120</h2>
-                                </div>
-                            </div>
-                            
-                            
-                           
-                            {/* <p className="sold_change_30_days">change is sold transactions over last 30 days compared to last 30 days</p>
-                            <p className="price_avg_30_days">numerical dollar value of change is avg listing price compared to last 30 days</p> */}
-                        </div>
-                        <button className="view_result">View Result Set</button>
-                    </div>
-                </div>
+                <ListingCard />
                
             </div>
             {/* this section is showing vehicles people recently requested results for */}
---
+++ frontend/my-app/src/components/UI/ListingCard/ListingCard.js
@@ -0,0 +1,52 @@
+import React,{useState} from "react"
+import "../../../styles.scss"
+// import module_css from './Navbar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import mclarenp1 from '../../../images/mclarenp1.jpg';
+
+
+
+export default function ListingCard(){
+
+   
+
+    return (
+      
+        <div className="listing_card">
+                    <div className="listing_card_content">
+                        <div className="listing_card_img_container">
+                            <img className="listing_card_img" src={mclarenp1} alt="" />
+                        </div>
+                        <div className="listing_card_icon_stats">
+                            <div className="listings_change_indicator" 
+                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
+                                <div className="listing_bottom_icons"> 
+                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
+                                    <h2 className="literal_value_change">120</h2>
+                                </div>
+                            </div>
+                            <div className="listings_change_indicator" 
+                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
+                                <div className="listing_bottom_icons"> 
+                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
+                                    <h2 className="literal_value_change">120</h2>
+                                </div>
+                            </div>
+                            <div className="listings_change_indicator" 
+                                 aria-label="indicator, green or red, showing change num of listings over current 30 days compared to last 30 days">
+                                <div className="listing_bottom_icons"> 
+                                    <h2 className="icon"><FontAwesomeIcon icon={["fas", "arrow-up"]} /></h2>
+                                    <h2 className="literal_value_change">120</h2>
+                                </div>
+                            </div>
+                            
+                            
+                           
+                            {/* <p className="sold_change_30_days">change is sold transactions over last 30 days compared to last 30 days</p>
+                            <p className="price_avg_30_days">numerical dollar value of change is avg listing price compared to last 30 days</p> */}
+                        </div>
+                        <button className="view_result">View Result Set</button>
+                    </div>
+                </div>
+    )    
+    }
