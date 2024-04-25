46c7a80b0cece4fd4e1df082125d874aa85fc392
ubuntu <ubuntu@ubuntu50gb.myguest.virtualbox.org>
2024-04-25 13:29:02 -0400

    api,psql,rmq services working and communicating

--- backend_copy/RabbitMQ_queues/scrape_producer.py
+++ backend_copy/RabbitMQ_queues/scrape_producer.py
@@ -13,7 +13,7 @@ RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
 #called from api - acts as producer - adds veh to queue as message
 def add_veh_to_queue(email_and_veh):
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_MQ_HOST))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
     channel = connection.channel()
 
     channel.queue_declare(queue='VEH_QUEUE',durable=True)
