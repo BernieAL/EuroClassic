3b09155866065cee9be9ee8f93a69cb518b87f8a
ubuntu <ubuntu@ubuntu50gb.myguest.virtualbox.org>
2024-06-05 14:36:42 -0400

    container_b populating db tables on launch

--- cont_B_entrypoint.sh
+++ cont_B_entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+set -e
+
+/wait-for-it-master/wait-for-it.sh -h pgsql -p 5432 -t 60
+
+python Postgres_logic/insert_data.py
+
 #start gunicorn with config
 exec gunicorn --config gunicorn_conf.py application:application
